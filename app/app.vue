5b905847d9e79af4d65582334d2ee7f7e9e52bda
Juan Lagunas <dev.jlagunas@gmail.com>
2025-12-14 14:16:37 +0900

    initial commit

---
+++ app/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <header>
+      <h1>Welcome to My Vue App</h1>
+    </header>
+    <main>
+      <nuxt-page />
+    </main>
+    <footer>
+      <p>&copy; 2024 My Vue App</p>
+    </footer>
+  </div>
+</template>
