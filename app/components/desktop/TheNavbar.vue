2fbf89badd18e9d716b10e078ff26d77bc8e2221
Juan Lagunas <dev.jlagunas@gmail.com>
2025-12-16 11:45:58 +0900

    chore: refactor styles in hero

--- app/components/desktop/TheNavbar.vue
+++ app/components/desktop/TheNavbar.vue
@@ -20,7 +20,7 @@ const isDark = computed(() => colorMode.value === "dark");
     <MobileNavHeader class="" />
     <nav
       aria-label="デスクトップナビゲーション"
-      class="hidden font-yomogi desktop:flex"
+      class="hidden font-yomogi desktop:flex flex-col"
     >
       <ul
         class="flex justify-around items-center border-b w-full border-b-dark-primary/20 py-4 dark:border-b-light-primary/20"
@@ -37,7 +37,9 @@ const isDark = computed(() => colorMode.value === "dark");
               name="heroicons:home"
               class="group-hover:rotate-5 group-hover:scale-110 transition-transform"
             />
-            <span>{{ $t("footer.nav.home") }}</span>
+            <span class="hover:font-bold lg:text-lg">{{
+              $t("footer.nav.home")
+            }}</span>
           </NuxtLink>
         </li>
 
@@ -53,7 +55,9 @@ const isDark = computed(() => colorMode.value === "dark");
               name="heroicons:information-circle"
               class="text-moko-blue group-hover:rotate-5 group-hover:scale-110 transition-transform"
             />
-            <span>{{ $t("footer.menu.start") }}</span>
+            <span class="hover:font-bold lg:text-lg">{{
+              $t("footer.menu.start")
+            }}</span>
           </NuxtLink>
         </li>
 
@@ -69,7 +73,9 @@ const isDark = computed(() => colorMode.value === "dark");
               name="ph:cat"
               class="text-soft-pink group-hover:rotate-5 group-hover:scale-110 transition-transform"
             />
-            <span>{{ $t("footer.nav.about") }}</span>
+            <span class="hover:font-bold lg:text-lg">{{
+              $t("footer.nav.about")
+            }}</span>
           </NuxtLink>
         </li>
 
@@ -85,7 +91,9 @@ const isDark = computed(() => colorMode.value === "dark");
               name="heroicons:face-smile"
               class="text-niko-purple group-hover:rotate-5 group-hover:scale-110 transition-transform"
             />
-            <span>{{ $t("footer.menu.characters") }}</span>
+            <span class="hover:font-bold lg:text-lg">{{
+              $t("footer.menu.characters")
+            }}</span>
           </NuxtLink>
         </li>
 
@@ -113,7 +121,9 @@ const isDark = computed(() => colorMode.value === "dark");
               name="heroicons:book-open"
               class="text-sunny-orange group-hover:rotate-5 group-hover:scale-110 transition-transform"
             />
-            <span>{{ $t("footer.nav.books") }}</span>
+            <span class="hover:font-bold lg:text-lg">{{
+              $t("footer.nav.books")
+            }}</span>
           </NuxtLink>
         </li>
 
@@ -129,7 +139,9 @@ const isDark = computed(() => colorMode.value === "dark");
               name="heroicons:puzzle-piece"
               class="text-okja-yellow group-hover:rotate-5 group-hover:scale-110 transition-transform"
             />
-            <span>{{ $t("footer.menu.kids") }}</span>
+            <span class="hover:font-bold lg:text-lg">{{
+              $t("footer.menu.kids")
+            }}</span>
           </NuxtLink>
         </li>
 
@@ -145,42 +157,146 @@ const isDark = computed(() => colorMode.value === "dark");
               name="heroicons:envelope"
               class="text-green-signifier group-hover:rotate-5 group-hover:scale-110 transition-transform"
             />
-            <span>{{ $t("footer.menu.contact") }}</span>
+            <span class="hover:font-bold lg:text-lg">{{
+              $t("footer.menu.contact")
+            }}</span>
           </NuxtLink>
         </li>
 
         <!-- 9. Language toggle -->
-        <li class="group">
-          <button
-            v-for="l in switchableLocales"
-            :key="l.code"
-            type="button"
-            @click="setLocale(l.code)"
-            :aria-label="l.name"
-            class="flex flex-col items-center hover:cursor-pointer"
-          >
-            <Icon
-              name="heroicons:language"
-              class="text-red-signifier group-hover:rotate-5 group-hover:scale-110 transition-transform"
-            />
-            <span>{{ l.name }}</span>
-          </button>
-        </li>
-
-        <!-- 10. Dark / Light toggle -->
-        <li class="group">
-          <button
-            type="button"
-            @click="toggleColorMode"
-            aria-label="テーマ切り替え"
-          >
-            <Icon v-if="isDark" name="heroicons:moon" />
-            <Icon v-else name="heroicons:sun" />
-          </button>
-        </li>
       </ul>
+      <div class="group flex items-center mt-4 w-fit mx-auto gap-8">
+        <button
+          v-for="l in switchableLocales"
+          :key="l.code"
+          type="button"
+          @click="setLocale(l.code)"
+          :aria-label="l.name"
+          class="flex flex-col items-center hover:cursor-pointer"
+        >
+          <span>{{ l.name }}</span>
+        </button>
+
+        <label class="switch">
+          <input
+            checked="true"
+            id="checkbox"
+            type="checkbox"
+            @click="toggleColorMode"
+          />
+          <span class="slider">
+            <div class="star star_1"></div>
+            <div class="star star_2"></div>
+            <div class="star star_3"></div>
+            <svg viewBox="0 0 16 16" class="cloud_1 cloud">
+              <path
+                transform="matrix(.77976 0 0 .78395-299.99-418.63)"
+                fill="#fff"
+                d="m391.84 540.91c-.421-.329-.949-.524-1.523-.524-1.351 0-2.451 1.084-2.485 2.435-1.395.526-2.388 1.88-2.388 3.466 0 1.874 1.385 3.423 3.182 3.667v.034h12.73v-.006c1.775-.104 3.182-1.584 3.182-3.395 0-1.747-1.309-3.186-2.994-3.379.007-.106.011-.214.011-.322 0-2.707-2.271-4.901-5.072-4.901-2.073 0-3.856 1.202-4.643 2.925"
+              ></path>
+            </svg>
+          </span>
+        </label>
+      </div>
     </nav>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+/* From Uiverse.io by JustCode14 */
+/* Theme Switch */
+/* The switch - the box around the slider */
+.switch {
+  font-size: 10px;
+  position: relative;
+  display: inline-block;
+  width: 4em;
+  height: 2em;
+  border-radius: 30px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #2a2a2a;
+  transition: 0.4s;
+  border-radius: 30px;
+  overflow: hidden;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 1.2em;
+  width: 1.2em;
+  border-radius: 20px;
+  left: 0.5em;
+  bottom: 0.5em;
+  transition: 0.4s;
+  transition-timing-function: cubic-bezier(0.81, -0.04, 0.38, 1.5);
+  box-shadow: inset 8px -4px 0px 0px #fff;
+}
+
+.switch input:checked + .slider {
+  background-color: #0077ff;
+}
+
+.switch input:checked + .slider:before {
+  transform: translateX(1.8em);
+  box-shadow: inset 15px -4px 0px 15px #ffcf48;
+}
+
+.star {
+  background-color: #fff;
+  border-radius: 50%;
+  position: absolute;
+  width: 5px;
+  transition: all 0.4s;
+  height: 5px;
+}
+
+.star_1 {
+  left: 2.5em;
+  top: 0.5em;
+}
+
+.star_2 {
+  left: 2.2em;
+  top: 1.2em;
+}
+
+.star_3 {
+  left: 3em;
+  top: 0.9em;
+}
+
+.switch input:checked ~ .slider .star {
+  opacity: 0;
+}
+
+.cloud {
+  width: 3.5em;
+  position: absolute;
+  bottom: -1.4em;
+  left: -1.1em;
+  opacity: 0;
+  transition: all 0.4s;
+}
+
+.switch input:checked ~ .slider .cloud {
+  opacity: 1;
+}
+</style>
